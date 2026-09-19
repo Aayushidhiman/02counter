eed5cf36eb08ca960db92dc50bec2c10b8bcb152
Aayushidhiman <dhimanayushi97@gmail.com>
2026-09-19 12:17:20 +0530

    Adding 02counter

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+
+
+function App() {
+
+    const[counter, setCounter] = useState(15) 
+
+    
+
+
+  // let counter = 15
+  
+  const addValue = () => {
+    if(counter<20){
+    setCounter(counter + 1);
+    console.log()
+    }
+  }
+    const removeValue = () => {
+      if(counter>0){
+    setCounter(counter - 1)
+    console.log(counter - 1);
+    }
+  }
+  
+
+  return (
+    <>
+    <h1>Chai aur react</h1>
+    <h2>Counter value: {counter}</h2>
+
+    <button
+    onClick={addValue}
+    >Add value {counter}</button>
+    <br/>
+    <button
+    onClick={removeValue}
+    >remove value{counter}</button>
+    <p>footer: {counter}</p>
+    
+    </>
+  )
+}
+
+export default App
